568657732b7a7194927735b56bf8ca8019d9ee6f
Ko Yubin <95890271+fgjkqm20@users.noreply.github.com>
2022-12-26 17:10:36 +0900

    [level 0] Title: 삼각형의 완성조건 (1), Time: 0.03 ms, Memory: 77.2 MB -BaekjoonHub

---
+++ 프로그래머스/lv0/120889. 삼각형의 완성조건 （1）/삼각형의 완성조건 （1）.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int solution(int[] sides) {
+        int max = 0;
+        int sum = 0;
+        
+        for(int i=0; i<3; i++){
+            if(max < sides[i]){
+                max = sides[i];
+            }
+            sum += sides[i];
+        }
+        
+        return (max < sum-max) ? 1 : 2;
+    }
+}
