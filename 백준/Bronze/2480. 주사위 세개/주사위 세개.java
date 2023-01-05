da0e4c4790468232c38ef17057d3cd314879c0ca
Ko Yubin <fgjkqm20@gmail.com>
2023-01-05 12:26:39 +0900

    Update 주사위 세개.java
    [Bronze IV] Title: 주사위 세개, Time: 112 ms, Memory: 12860 KB -BaekjoonHub

--- 백준/Bronze/2480. 주사위 세개/주사위 세개.java
+++ 백준/Bronze/2480. 주사위 세개/주사위 세개.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class Main{
+public class Main{ 
 	public static void main(String[] args){
 		int[] nums = new int[3];
 		Scanner scanner = new Scanner(System.in);
