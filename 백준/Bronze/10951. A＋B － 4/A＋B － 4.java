8c674aa13a7e300115de35c394d4455e0b2fb8ad
Ko Yubin <fgjkqm20@gmail.com>
2023-01-10 06:56:50 +0900

    [Bronze V] Title: A+B - 4, Time: 132 ms, Memory: 13152 KB -BaekjoonHub

---
+++ 백준/Bronze/10951. A＋B － 4/A＋B － 4.java
@@ -0,0 +1,22 @@
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Scanner;
+import java.util.InputMismatchException;
+
+public class Main{ 
+    public static void main(String[] args){ 
+        Scanner scanner = new Scanner(System.in);  
+        
+        List<Integer> A = new ArrayList<Integer>();
+        List<Integer> B = new ArrayList<Integer>();
+        
+        while(scanner.hasNextInt()){
+            A.add(scanner.nextInt());
+            B.add(scanner.nextInt());
+        }
+        
+        for(int i=0; i<A.size(); i++){
+            System.out.println(A.get(i) + B.get(i));
+        }
+    } 
+}
