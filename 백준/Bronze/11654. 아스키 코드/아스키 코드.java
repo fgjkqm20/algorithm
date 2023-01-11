443136db83edb2f4cc8f1cdf0b7e14ac1e60d06c
Ko Yubin <fgjkqm20@gmail.com>
2023-01-12 05:57:45 +0900

    [Bronze V] Title: 아스키 코드, Time: 104 ms, Memory: 12776 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,9 @@
+import java.util.Scanner;
+
+public class Main{ 
+    public static void main(String[] args){ 
+        Scanner scanner = new Scanner(System.in); 
+        int ascll = (int)scanner.next().charAt(0);
+        System.out.println(ascll);
+    } 
+}
