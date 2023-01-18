7dae492d7464031069e83c4aae290d66f4b9d3ae
Ko Yubin <fgjkqm20@gmail.com>
2023-01-18 11:19:30 +0900

    [Bronze III] Title: 별 찍기 - 4, Time: 288 ms, Memory: 17612 KB -BaekjoonHub

---
+++ 백준/Bronze/2441. 별 찍기 － 4/별 찍기 － 4.java
@@ -0,0 +1,18 @@
+import java.util.Scanner;
+
+public class Main{ 
+    public static void main(String[] args){ 
+        Scanner scanner = new Scanner(System.in);  
+        int n = scanner.nextInt();
+        
+		for(int i=n; i>=1; i--) {
+			for(int j=0; j<n-i; j++) {
+				System.out.print(" ");
+			}
+			for(int j=0; j<i; j++) {
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+    } 
+}
