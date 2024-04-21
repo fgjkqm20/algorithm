c96b2a666fc22735b3355e364b68a12944ccbfa7
고유빈 <fgjkqm20@gmail.com>
2024-04-21 15:43:02 +0900

    [level 0] Title: 뒤에서 5등까지, Time: 0.41 ms, Memory: 73.1 MB -BaekjoonHub

---
+++ 프로그래머스/0/181853. 뒤에서 5등까지/뒤에서 5등까지.java
@@ -0,0 +1,10 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[] solution(int[] num_list) {
+        Arrays.sort(num_list);
+        int[] answer = Arrays.copyOfRange(num_list, 0, 5);
+        
+        return answer;
+    }
+}
