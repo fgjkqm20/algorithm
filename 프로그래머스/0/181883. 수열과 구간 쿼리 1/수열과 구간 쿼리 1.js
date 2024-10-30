4764ad4753d5fd0e7a2b0bbc0858214d5b6cf22e
고유빈 <fgjkqm20@gmail.com>
2024-10-30 15:52:47 +0900

    [level 0] Title: 수열과 구간 쿼리 1, Time: 2.67 ms, Memory: 35.6 MB -BaekjoonHub

---
+++ 프로그래머스/0/181883. 수열과 구간 쿼리 1/수열과 구간 쿼리 1.js
@@ -0,0 +1,9 @@
+function solution(arr, queries) {
+    queries.forEach(query => {
+        for(let i = query[0]; i <= query[1]; i++) {
+            arr[i] += 1;
+        }
+    })
+    
+    return arr;
+}
