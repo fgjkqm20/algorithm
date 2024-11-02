6e299cd2c6c75740e183e7842c57a0f1aa290d05
고유빈 <fgjkqm20@gmail.com>
2024-11-02 09:04:46 +0900

    [level 0] Title: qr code, Time: 0.19 ms, Memory: 33.4 MB -BaekjoonHub

---
+++ 프로그래머스/0/181903. qr code/qr code.js
@@ -0,0 +1,11 @@
+function solution(q, r, code) {
+    let answer = '';
+    
+    for(let i = 0; i < code.length; i++) {
+        if(i % q === r) {
+            answer += code.charAt(i);
+        }
+    }
+    
+    return answer;
+}
