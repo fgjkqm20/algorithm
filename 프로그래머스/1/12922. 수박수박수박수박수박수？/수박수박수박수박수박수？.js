ce3420c28b00ca4e4a623e38faff80c4571ec413
고유빈 <fgjkqm20@gmail.com>
2024-11-18 06:11:21 +0900

    [level 1] Title: 수박수박수박수박수박수?, Time: 0.50 ms, Memory: 33.7 MB -BaekjoonHub

---
+++ 프로그래머스/1/12922. 수박수박수박수박수박수？/수박수박수박수박수박수？.js
@@ -0,0 +1,13 @@
+function solution(n) {
+    let answer = '';
+    
+    for(let i = 1; i <= n; i++) {
+        if(i % 2) {
+            answer += '수'
+        } else {
+            answer += '박'
+        }
+    }
+    
+    return answer;
+}
